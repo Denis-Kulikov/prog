4123889a41b3ea9f1854c0554b43d66e4ec0a202
Kulikov Denis <denis_kulikov_a_2003@mail.ru>
2022-06-17 00:40:39 +0700

    Fix moving

--- cformat.h
+++ cformat.h
@@ -1,3 +1,8 @@
+int previous_symbol(fragment_code** _code, char** _s);
+int next_symbol(fragment_code** code, char** s);
+int scmp_command(char* str1, char* str2);
+void delete_symbol(fragment_code* code, char* pos);
+
 char* get_start_symbol(fragment_code* code);
 fragment_code* creating_fragment(fragment_code* f);
 int read_code(FILE* f, fragment_code* code);
